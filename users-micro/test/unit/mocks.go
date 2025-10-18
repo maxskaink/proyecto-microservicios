ffc6348b82ef659e4c65ad0fb783dcd611f02c61
maxskaink <mangelcvivas@unicauca.edu.co>
2025-10-17 22:30:07 -0500

    test: add mock UserRepository and update unit tests for improved coverage
    - Implement `NewMockUserService` in mock repository.
    - Add `UpdateRol` method to mock UserRepository.
    - Enhance tests to include error type assertions using `errors.As`.

--- users-micro/test/unit/mocks.go
+++ users-micro/test/unit/mocks.go
@@ -1,6 +1,7 @@
 package unit_test
 
 import (
+	"github.com/maxskaink/proyecto-microservicios/users-micro/internal/db/repositories"
 	"github.com/maxskaink/proyecto-microservicios/users-micro/internal/dto"
 	"github.com/stretchr/testify/mock"
 )
@@ -8,6 +9,10 @@ import (
 // Crear un mock del repositorio
 type MockUserRepository struct {
 	mock.Mock
+}
+
+func NewMockUserService() repositories.UserRepository {
+	return &MockUserRepository{}
 }
 
 func (m *MockUserRepository) Create(u *dto.UserRequest) (*dto.UserResponse, error) {
@@ -36,6 +41,14 @@ func (m *MockUserRepository) FindByUUID(uuid string) (*dto.UserResponse, error) 
 
 func (m *MockUserRepository) Update(id string, u *dto.UserRequest) (*dto.UserResponse, error) {
 	args := m.Called(id, u)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*dto.UserResponse), args.Error(1)
+}
+
+func (m *MockUserRepository) UpdateRol(id string, rol string) (*dto.UserResponse, error) {
+	args := m.Called(id, rol)
 	if args.Get(0) == nil {
 		return nil, args.Error(1)
 	}

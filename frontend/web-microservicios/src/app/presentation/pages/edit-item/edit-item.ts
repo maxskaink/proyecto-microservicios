d3417e24a2c17b693729e18370d039e8d4bcab2a
Santiago Escandon <santiescandon1@gmail.com>
2025-11-28 02:16:02 -0500

    se refactoriza el formulario en un componente template

---
+++ frontend/web-microservicios/src/app/presentation/pages/edit-item/edit-item.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-item',
+  imports: [],
+  templateUrl: './edit-item.html',
+  styleUrl: './edit-item.css',
+})
+export class EditItem {
+
+}

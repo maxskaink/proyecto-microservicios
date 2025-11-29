9b8dde34f112a886d3046f8a5b48fe4ce3a3f048
Santiago Escandon <santiescandon1@gmail.com>
2025-11-29 15:30:49 -0500

    funcionalidad listar usuarios implementada

--- frontend/web-microservicios/src/app/presentation/pages/view-panel-users/view-panel-users.ts
+++ frontend/web-microservicios/src/app/presentation/pages/view-panel-users/view-panel-users.ts
@@ -1,38 +1,52 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnChanges, SimpleChanges } from '@angular/core';
+import { ChangeDetectorRef, Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { UserBox } from '../../components/user-box/user-box';
 import { UserResponseBack } from '../../../Models/UserReponseBack';
 import { Header } from '../../templates/header/header';
 import { UsersService } from '../../../service/users.service';
 import Swal from 'sweetalert2';
+import { IsLoading } from '../../components/is-loading/is-loading';
+import { LoadingService } from '../../../service/loading-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-view-panel-users',
-  imports: [CommonModule, UserBox, Header],
+  imports: [CommonModule, UserBox, Header, IsLoading],
   templateUrl: './view-panel-users.html',
   styleUrl: './view-panel-users.css',
 })
-export class ViewPanelUsers implements OnChanges{
+export class ViewPanelUsers implements OnInit{
   public users!: UserResponseBack[];
   public producers!: UserResponseBack[];
   public admins!: UserResponseBack[];
   constructor(
     private userService: UsersService,
+    private cdr:  ChangeDetectorRef,
+    private router: Router,
+    private isLoading: LoadingService
   ) {}
-  ngOnChanges(changes: SimpleChanges): void {
+  ngOnInit(): void {
     this.loadUsers();
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    
   }
   /**
    * cargar usuarios desde el servicio, todos los usuarios existentes del tenant
    */
   loadUsers() {
+    this.isLoading.show('Cargando usuarios...');
   this.userService.getAllUsers().subscribe({
     next: (data: UserResponseBack[]) => {
       this.users = data.filter(user => user.rol !== 'client');
       this.producers = this.users.filter(user => user.rol === 'producer');
       this.admins = this.users.filter(user => user.rol === 'admin');
+      this.cdr.detectChanges();
+      this.isLoading.hide();
     },
     error: (error) => {
+      this.cdr.detectChanges();
+      this.isLoading.hide();
       console.error('Error fetching users:', error);
       Swal.fire({
         icon: 'error',
@@ -46,6 +60,10 @@ export class ViewPanelUsers implements OnChanges{
     }
   });
 }
+    onAction(id: string){
+      console.log('Acción recibida para el usuario con ID:', id);
+      
+    }
 
 
 } 
